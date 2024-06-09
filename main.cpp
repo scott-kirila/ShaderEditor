da0539b3556f1b2e3c6c1faf8bebb5c863ae9d18
Scott Kirila <68308305+scott-kirila@users.noreply.github.com>
2024-06-08 20:21:34 -0400

    Minor syntax change.

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 #include "RenderingOpenGL.h"
 #include "Window.h"
 
-constexpr int g_ViewWidth = 1920;
-constexpr int g_ViewHeight = 1080;
+constexpr int VIEW_WIDTH = 1920;
+constexpr int VIEW_HEIGHT = 1080;
 #if _WIN64
 auto g_GlslVersion = "#version 460\n";
 #elif __APPLE__
@@ -14,9 +14,9 @@ auto g_GlslVersion = "#version 410\n";
 #endif
 
 int main() {
-    const auto window = std::make_shared<Window>(g_ViewWidth, g_ViewHeight);
+    const auto window = std::make_shared<Window>(VIEW_WIDTH, VIEW_HEIGHT);
     const auto renderer = std::make_shared<Rendering::OpenGL>(window.get(), g_GlslVersion);
-    auto gui = GUI(window, renderer, g_ViewWidth, g_ViewHeight, g_GlslVersion);
+    auto gui = GUI(window, renderer, VIEW_WIDTH, VIEW_HEIGHT, g_GlslVersion);
 
     gui.Loop();
 
